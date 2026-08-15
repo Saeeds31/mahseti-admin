5335561beb12f7946540456924d8a275a78c79e0
saeed dehghan <saeeddehghans31@gmail.com>
2026-08-15 10:46:56 +0330

    s

--- src/components/shared/sideBar.vue
+++ src/components/shared/sideBar.vue
@@ -386,7 +386,7 @@ const menuItems = ref([
   },
   {
     name: "تنظیمات",
-    permissions: ['settings_view'],
+    permissions: ['setting_view'],
     icon: "bi-gear",
     link: "/settings",
   },
